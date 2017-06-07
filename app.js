739d717d843882dbbf683983b316c76baabbc556
Scylol <djd3dobbin@gmail.com>
2017-06-07 19:27:31 -0400

    fixed hovers, and header sizing issue

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 $('.contact').on('click',  function(){
   $('.contact-form').toggleClass('hidden');
+  $('.about').toggleClass('hidden');
+  if($(this).text() === 'Contact Me') {
+    $(this).text('Hide');
+  }
+  else {
+    $(this).text('Contact Me');
+  }
+ 
 });
+
